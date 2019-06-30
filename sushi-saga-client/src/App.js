770bc993cd0571d69fcb6817dc1e32883e8215ee
Preston Elliott <preston.elliott24@gmail.com>
2019-06-30 15:30:03 -0400

    done

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -2,16 +2,60 @@ import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
-// Endpoint!
-const API = "http://localhost:3000/sushis"
+const sushiLimit = 4
+const API = `http://localhost:3000/sushis?_limit=${sushiLimit}&_page=`
 
 class App extends Component {
+
+  state = {
+    page: 1,
+    wallet: 100,
+    sushis: [],
+    eaten: []
+  }
+
+  fetchSushi = () => {
+    fetch(API+this.state.page)
+    .then(res => res.json())
+    .then(res => this.setState({
+      page: this.state.page + 1,
+      sushis: res
+    }))
+  }
+
+  componentDidMount() {
+    this.fetchSushi()
+  }
+
+  eatSushi = (sushi) => {
+    if(sushi.price <= this.state.wallet) {
+      this.setState({
+        wallet: this.state.wallet - sushi.price,
+        eaten: [...this.state.eaten, sushi.id]
+      })
+    } 
+    else {
+       alert("You can't afford that sushi!")
+    }
+  }
+
+  moreSushi = () => {
+    this.fetchSushi()
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiContainer 
+          moreSushi={this.moreSushi} 
+          eatSushi={this.eatSushi} 
+          sushis={this.state.sushis} 
+          eaten={this.state.eaten}
+        />
+        <Table 
+          wallet={this.state.wallet} 
+          eaten={this.state.eaten}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -2,21 +2,23 @@ import React, { Fragment } from 'react'
 
 const Sushi = (props) => {
   return (
-    <div className="sushi">
-      <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          true ?
-            null
-          :
-            <img src={/* Give me an image source! */} width="100%" />
-        }
+    <Fragment>
+      <div className="sushi">
+        <div className="plate"> 
+          { props.eaten ? null : 
+          <img onClick={(e) => 
+            props.eatSushi(props.sushi)} 
+            src={props.sushi.img_url} 
+            id={props.sushi.id} 
+            width="100%"
+            alt="Sushi!" />
+          }
+        </div>
+        <h4 className="sushi-details">
+          {props.sushi.name} - ${props.sushi.price}
+        </h4>
       </div>
-      <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
-      </h4>
-    </div>
+    </Fragment>
   )
 }
 
